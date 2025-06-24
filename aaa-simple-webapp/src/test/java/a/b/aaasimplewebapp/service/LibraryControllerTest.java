3f6fab5b1f298f32d0d45b77d2e59befe97c6794
elkurto <kurt.buehner@gmail.com>
2025-06-24 13:44:53 -0400

    added tests for LibraryController

---
+++ aaa-simple-webapp/src/test/java/a/b/aaasimplewebapp/service/LibraryControllerTest.java
@@ -0,0 +1,71 @@
+package a.b.aaasimplewebapp.service;
+
+
+import a.b.aaasimplewebapp.domain.Book;
+import a.b.aaasimplewebapp.domain.ConstRestUri;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.json.JsonCompareMode;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LibraryController.class)
+public class LibraryControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @MockitoBean
+  private LibraryService libraryService;
+
+  @Test
+  void testIndex() throws Exception {
+    String expected_body = "{\"segment\":\"/library\",\"status\":\"success\"}";
+    mockMvc.perform(MockMvcRequestBuilders.get(ConstRestUri.LIBRARY))
+            .andExpect(status().isOk())
+            .andExpect(content().json( expected_body, JsonCompareMode.LENIENT))
+            .andExpect(content().contentTypeCompatibleWith( MediaType.APPLICATION_JSON));
+
+  }
+
+  @Test
+  void testGetOneByUuid() throws Exception {
+    // given
+    UUID uuid = UUID.randomUUID();
+    Book book = new Book(uuid, "foo","bar", 123456);
+
+    when(libraryService.findById(uuid)).thenReturn(book);
+
+    // when
+    mockMvc.perform(MockMvcRequestBuilders.get(ConstRestUri.LIBRARY_BOOK + "/"+ uuid.toString()))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(uuid.toString()))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("foo"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("bar"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.publishedDateMillis").value(123456))
+
+    ;
+
+  }
+
+  @Test
+  void testShouldReturn404WhenNotFound() throws Exception {
+    UUID uuid = UUID.randomUUID();
+    when(libraryService.findById(uuid)).thenReturn(null);
+
+    mockMvc.perform(MockMvcRequestBuilders.get(ConstRestUri.LIBRARY_BOOK +"/"+ uuid.toString()))
+            .andExpect(status().isNotFound());
+  }
+}
